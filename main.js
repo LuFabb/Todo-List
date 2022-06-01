e6e5edd003f20ca8a32e6b889fc1c9c9c325d75a
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-01 16:16:19 -0500

    updating the application to make the data persist

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 
 const render = () => {
     const todoList = document.getElementById('todo-list');
-    const todosTemplate = todos.map(t =>  '<li>' + t + '</li>');
+    const todosTemplate = todos.map(t => '<li>' + t + '</li>');
     todoList.innerHTML = todosTemplate.join('');
     const elementos = document.querySelectorAll('#todo-list li');
     elementos.forEach((elemento, i) => {
     elemento.addEventListener('click', () => {
     elemento.parentNode.removeChild(elemento)
     todos.splice(i, 1)
+    updadeTodos(todos)
     render()
   })
 })
 }
 
+const updadeTodos = () => {
+  const todoString = JSON.stringify(todos)
+  localStorage.setItem('todo', todoString)
+}
+
 
 window.onload = () => {
-  const form = document.getElementById('todo-form');
-  form.onsubmit = (e) => {
-    e.preventDefault();
+    render()
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e) => {
+     e.preventDefault();
     const todo = document.getElementById('todo');
     const todoText = todo.value;
     todo.value = '';
     todos.push(todoText);
+    updadeTodos(todos)
     render()
   }
 }
